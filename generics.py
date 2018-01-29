8f367590de8eadc1413c8a7a90b148ed86e1a632
Todd Wickizer <toddwickizer@gmail.com>
2018-01-29 11:55:45 -0700

    Wisconsin Start

---
+++ generics.py
@@ -0,0 +1,58 @@
+import sys
+import argparse
+import logging
+import sys
+import pexpect
+import time
+import os
+
+
+
+def create_shell_and_connect_generic(args):
+    """Creates a shell, makes sure the key is the right permission and then 
+    attempts to connect, rerturns shell instance """
+
+
+    pexpect.spawn("ssh-keyscan "+args.hostname+" >> ~/.ssh/known_hosts")
+    time.sleep(5)
+
+    os.chmod(str(args.pkey), 0600)
+
+    logging.debug("Starting SSH Instance... this may take a minute.")
+    child = pexpect.spawn("ssh -i" +str(args.pkey)+ " " + str(args.user) +
+                              "@" + str(args.hostname) + "")
+
+    #   There is a chance on first time to display this message. we need to handle it.
+    index = child.expect(['Are you sure you want to continue connecting*',
+                          pexpect.EOF, pexpect.TIMEOUT], timeout=5)
+    logging.debug(child.before)
+
+    #   If the index == 0, that means the message was present.
+    if index == 0:
+        child.sendline("yes")
+        time.sleep(2)
+
+    return child
+
+def create_and_submit_generic(child, args, expected):
+    """Creates and submits the file with the the open ssh instance"""
+
+    with open(args.script, 'r') as myfile:
+        for line in myfile:
+            line = line.replace('\r', '').replace('\n', '').replace('\\','\\\\').replace('$', '\$').replace('"', '\\\"')
+            logging.debug(line)
+            child.sendline("echo \"" + line + "\" >> mysubmit.file")
+            child.expect(expected)
+
+        # submit file to queue here.
+        if (args.submit):
+            child.sendline(args.submit + " mysubmit.file")
+        else:
+            child.sendline("chmod +x mysubmit.file")
+            child.sendline("nohup ./mysubmit.file &")
+
+        child.expect(expected)
+        logging.debug(child.before)
+
+        child.expect(expected)
+        logging.debug(child.before)
